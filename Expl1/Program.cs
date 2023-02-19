a006333c58d917b251ece421cf3debfa4830cd91
Artem <aromanov.b@ya.ru>
2023-02-19 12:31:11 +0300

    Method_4

--- Expl1/Program.cs
+++ Expl1/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//void Method1()
+//{
+ //   System.Console.WriteLine("Автор ФИО");
+//}
+
+//Method1();
+
+// Method 2
+
+//void Method2(string msg)
+//{
+ //   Console.WriteLine(msg);
+//}
+//Method2(msg: "Текст сообщения");
+
+//void Method21(string msg, int count)
+///{
+ //   int i = 0;
+ //   while (i < count)
+   // {
+    //    Console.WriteLine(msg);
+    //    i++;
+   // }
+//}
+//Method21(msg:"Текст", 4);
+//Method21(count:"Новый Текст", 4);
+
+//int Method3()
+//{
+//    return DateTime.Now.Year;
+//}
+
+//int year = Method3();
+//Console.WriteLine(year);
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+        while (i < count)
+        {
+            result = result + c;
+            i++;
+        }
+    return result;
+}
+
+string res = Method4(10, "ASDF ");
+Console.WriteLine(res);
+
