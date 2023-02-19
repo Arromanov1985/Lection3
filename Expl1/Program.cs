05d11d2dad03c119eb835932d5569e5d8cea4d0e
Artem <aromanov.b@ya.ru>
2023-02-19 13:43:52 +0300

    111

--- Expl1/Program.cs
+++ Expl1/Program.cs
@@ -87,34 +87,79 @@
 // а большие “С” маленькими “с”.
 // Ясна ли задача?
 
-//string text = “— Я думаю, — сказал князь, улыбаясь, — что,”
-//● ”ежели бы вас послали вместо нашего милого Винценгероде,”
-//● ”вы бы взяли приступом согласие прусского короля.”
-//● ”Вы так красноречивы. Вы дадите мне чаю?”;
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
 
 // string s = "qwerty"
 //               012345
 // s[3]  // r
 
 
-using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
 ///{
-string Replace (string text, char oldValue, char newValue )
+
+//string Replace (string text, char oldValue, char newValue )
+//{
+  //  string result = String.Empty;
+
+    //int length = text.Length;
+    //for ( int i = 0; i < length; i++)
+    //{
+      //  if(text[i] == oldValue) result = result + $"{newValue}";
+        //else result = result + $"{text[i]}";
+    //}
+
+
+
+    //return result;
+//}
+
+
+//string newText = Replace (text,' ','|');
+
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К');
+//Console.WriteLine(newText);
+
+
+// Функции в программирование Массивы
+
+int[] arr = {1, 5, 6, 7, 8, 9, 0, 3, 3, 4, 5, 6, 6};
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
+    int count = array.Length;
 
-    int length = text.Length;
-    for ( int i = 0; i < length; i++)
+    for (int i = 0; i < count; i++)
     {
-        if(text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]}");
     }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length ; i++)
+    { 
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
 
 
 
-    return result;
+        int temporary = array[i];
+        array[i] = array [minPosition]; 
+        array[minPosition] = temporary;
+        }
 }
 
 
-string newText = Replace(text,'  ',  '|');
-Console.WriteLine(newText);
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);  // домашнее задание сделать сортировку наоборот от меньшего к большему
